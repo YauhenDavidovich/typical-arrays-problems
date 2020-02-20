4c7a570a3685808939c6b88ea49d51d5f3d8c770
Шурхай Алексей Сергеевич <a.shurkhai@ziko.loc>
2020-02-20 13:04:08 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }  else if (array.length == 0) {
+    return 0
+  } else {
+  let len = array.length;
+  let min = Infinity;
+  while (len--) {  //len is bigger then 0
+    if (array[len] < min) {
+      min = array[len];
+    }
+  }
+  return min;
+  }  
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }else  if (array.length == 0) {
+    return 0
+  } else {
+  let len = array.length;
+  let max = -Infinity;
+  while (len--) {  //len is bigger then 0
+    if (array[len] > max) {
+      max = array[len];
+    }
+  }
+  return max;
+  }  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) {
+    return 0
+  }else  if (array.length == 0) {
+    return 0
+  } else {
+    let argumentsSum = 0
+    for (let i = 0; i < array.length; i++) {
+      argumentsSum += array[i]
+    } return argumentsSum/array.length
+  } 
 }
